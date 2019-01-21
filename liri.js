5f6d2c4ae019d21842cea1a86220a7d03694e7d8
garcila <germanarcila@gmail.com>
2019-01-21 10:11:00 -0500

    install libraries, add switch, concert, song and movie function

---
+++ liri.js
@@ -0,0 +1,86 @@
+require(`dotenv`).config();
+
+const KEYS = require(`./keys.js`);
+const axios = require(`axios`);
+const Spotify = require(`node-spotify-api`);
+const moment = require(`moment`);
+
+const userInput = process.argv;
+const command = process.argv[2];
+
+const concert = (userInput) => {
+	let artist = userInput.length < 4 ? 'Bokassa' : userInput.splice(3, 1);
+	let url = `https://rest.bandsintown.com/artists/${artist}/events?app_id=${KEYS.bandsInTown.key}`;
+	axios.get(url).then((res) => {
+		let data = res.data[0];
+		let nextEvent = {
+			Band: data.lineup[0],
+			Venue: data.venue.name,
+			City: data.venue.city,
+			Country: data.venue.country,
+			Date: moment(data.datetime).format('MM/DD/YYYY')
+		};
+		console.table(nextEvent);
+	});
+};
+
+const song = (userInput) => {
+	let songTitle = userInput.length < 4 ? `La vie en rose` : userInput.splice(3, 1);
+	const spotify = new Spotify({
+		id: KEYS.spotify.id,
+		secret: KEYS.spotify.secret
+	});
+	spotify.search({ type: `track`, query: `${songTitle}` }).then((res) => {
+		const data = res.tracks.items[0];
+		let songInfo = {
+			Artist: data.album.artists[0].name,
+			Song: data.name,
+			OnSpotify: data.external_urls.spotify,
+			Album: data.album.name,
+			Year: moment(data.album.release_date).format(`DD/MM/YYYY`)
+		};
+		console.table(songInfo);
+	});
+};
+
+const movie = (userInput) => {
+	let movieTitle = userInput.length < 4 ? 'Tron' : userInput.splice(3, 1);
+	let url = `http://www.omdbapi.com/?apikey=${KEYS.omdb.key}&t=${movieTitle}`;
+	axios
+		.get(url)
+		.then((res) => {
+			let movie = res.data;
+			let movieInfo = {
+				Title: movie.Title,
+				Year: movie.Year,
+				Rating: movie.Rated,
+				RottenTomatoes: movie.Ratings[1].Value,
+				Country: movie.Country,
+				Language: movie.Language,
+				Plot: movie.Plot,
+				Actors: movie.Actors
+			};
+			console.table(movieInfo);
+		})
+		.catch((err) => {
+			throw `We had a problem. ${err}`;
+		});
+};
+
+switch (command) {
+	case `concert-this`:
+		concert(userInput);
+		break;
+	case `spotify-this-song`:
+		song(userInput);
+		break;
+	case `movie-this`:
+		movie(userInput);
+		break;
+	case `do-what-it-says`:
+		doWhatItSays();
+		break;
+	default:
+		console.log(`Sorry I do not recognize that command`);
+		break;
+}
---
+++ todo.md
@@ -0,0 +1,34 @@
+# ToDos
+
+- [x] Add keys for all three APIs
+  - [x] Spotify
+  - [x] OMDB
+  - [x] Bands in Town
+- [ ] Install npm packages
+  - [x] Axios
+  - [x] Node-Spotify-API
+  - [x] Moment
+  - [x] DotEnv
+  - [ ] inquirer.js - **bonus**
+- [x] Require packages in your liri.js file
+- [x] Require your keys from keys.js
+- [x] Capture user input
+  - [x] Store it in a variable userInput
+  - [x] parse userInput and get
+    - [x] command
+      - [x] Depending on the command
+        - [x] store different actions
+- [x] Include default case for each command
+- [x] Create functions for each case
+- [x] Fetch action using Axios
+- [x] Display response in the terminal according to requirements
+- [ ] Create README.MD  
+- [ ] Handle multiple artists in spotify query
+- [ ] Do do-what-it-says
+  - [ ] require fs
+
+## Bonus
+- [ ] Output data to log.txt file
+- [ ] Capture user input using inquirer
+  - [ ] First one is command
+  - [ ] Second is the action on the **command**
